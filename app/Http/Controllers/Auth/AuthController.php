b7cdac46e6a403b09f23d00a26e0bc848d47d3dd
Cesar Amaro <cesar.amaro@20d.mx>
2018-06-27 13:32:19 -0500

    update login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\User;
+use App\Subscription;
 use Validator;
 use ChargeBee_Subscription;
 use ChargeBee_PortalSession;
@@ -14,102 +15,108 @@ use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Registration & Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Registration & Login Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the registration of new users, as well as the
+	| authentication of existing users. By default, this controller uses
+	| a simple trait to add these behaviors. Why don't you explore it?
+	|
+	*/
 
-    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+	use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-    /**
-     * Where to redirect users after login / registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
+	/**
+	 * Where to redirect users after login / registration.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = '/';
 
-    /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
-    }
+	/**
+	 * Create a new authentication controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+	}
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
-        ]);
-    }
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array  $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function validator(array $data)
+	{
+		return Validator::make($data, [
+			'email' => 'required|email|max:255|unique:users',
+			'password' => 'required|min:6|confirmed',
+		]);
+	}
 
-    public function authenticate_CB (Request $request) {
-        $result = ChargeBee_PortalSession::activate($request->auth_session_id, array(
-                "token" => $request->auth_session_token));
+	public function authenticate_CB (Request $request) {
+		$result = ChargeBee_PortalSession::activate($request->auth_session_id, array(
+				"token" => $request->auth_session_token));
 
-        // $linked_customers = $result->portalSession()->linkedCustomers;
-        // $cb_customer_email = $linked_customers[0]->email;
-        // $customer_id = $result->portalSession()->customerId;
-        // $listOfSubscription = ChargeBee_Subscription::subscriptionsForCustomer($customer_id);
-        // foreach ($listOfSubscription as $value) {
-        //     $subscriptionDetails[] = $value;
-        // }        
-        // $subscriptionDetails = $subscriptionDetails[0];
-        // $subscription = $subscriptionDetails->subscription();
-        // $this->setSubscriptionId($subscription->id);
-    }
+		$customer_id = $result->portalSession()->customerId;
+		$listOfSubscription = ChargeBee_Subscription::subscriptionsForCustomer($customer_id);
+		$cb_subscriptions = ChargeBee_Subscription::all(array(
+			"limit" => 1,
+			"customerId[is]" => $customer_id));
+		foreach ($cb_subscriptions as $cb_subscription) {
+			$subscription = $cb_subscription->subscription();
+		}
+		$user = Subscription::where('subscription_id', '=', $subscription['id'])->first();
+		Auth::login($user);
+		
+		return redirect('dashboard');
+	}
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        $cb_obj = ChargeBee_Customer::all(array(
-            "limit" => 1,
-            "email[is]" => $data['email'],
-        ));
-        foreach ($cb_obj as $cb_customer) {
-            $customer = $cb_customer->customer();
-            $cb_subs = ChargeBee_Subscription::all(array(
-                "limit" => 1, 
-                "customerId[is]" => $customer->id,
-                "status[is]" => 'active',
-            ));
-            if (count($cb_subs) > 0) {
-                foreach ($cb_subs as $cb_sub) {
-                    $sub = $cb_sub->subscription();
-                }
-            } else {
-                $errors = ['Chargebee customer does not exists!'];
-                return view('register', compact('errors'));
-            }
-        }
+	public function logout () {
+		ChargeBee_PortalSession::logout($request->auth_session_id);
+	}
 
-        // return User::create([
-        //     'name' => $data['name'],
-        //     'last_name' => $data['last_name'],
-        //     'email' => $data['email'],
-        //     'phone' => $data['phone'],
-        //     'password' => bcrypt($data['password']),
-        // ]);
-    }
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array  $data
+	 * @return User
+	 */
+	protected function create(array $data)
+	{
+		$cb_obj = ChargeBee_Customer::all(array(
+			"limit" => 1,
+			"email[is]" => $data['email'],
+		));
+		foreach ($cb_obj as $cb_customer) {
+			$customer = $cb_customer->customer();
+			$cb_subs = ChargeBee_Subscription::all(array(
+				"limit" => 1, 
+				"customerId[is]" => $customer->id,
+				"status[is]" => 'active',
+			));
+			if (count($cb_subs) > 0) {
+				foreach ($cb_subs as $cb_sub) {
+					$sub = $cb_sub->subscription();
+				}
+			} else {
+				$errors = ['Chargebee customer does not exists!'];
+				return view('register', compact('errors'));
+			}
+		}
+
+		// return User::create([
+		//     'name' => $data['name'],
+		//     'last_name' => $data['last_name'],
+		//     'email' => $data['email'],
+		//     'phone' => $data['phone'],
+		//     'password' => bcrypt($data['password']),
+		// ]);
+	}
 }
