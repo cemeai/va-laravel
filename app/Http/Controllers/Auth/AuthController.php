86737bf553b4597852c20b2d469dec2206068225
Cesar Amaro <cesar.amaro@20d.mx>
2018-06-29 15:05:20 -0500

    test account

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -83,6 +83,7 @@ class AuthController extends Controller
 	}
 
 	public function logout () {
+		print_r(session('auth_session_id')); exit();
 		ChargeBee_PortalSession::logout(session('auth_session_id'));
 		Auth::logout();
 		return redirect('dashboard');
